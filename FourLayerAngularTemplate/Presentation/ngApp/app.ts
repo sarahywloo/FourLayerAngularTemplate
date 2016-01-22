2f5ffd5ccb00f2a330c478c1f3ffb5b6dd78a2ed
Sarah Kim <sarahywloo@gmail.com>
2016-01-22 13:20:37 -0600

    A multi-folder Four Layer Angular Template

---
+++ FourLayerAngularTemplate/Presentation/ngApp/app.ts
@@ -0,0 +1,9 @@
+﻿namespace $safeprojectname$ {
+    angular.module("$safeprojectname$", ['ngRoute', 'ui.bootstrap'])
+        .config(function ($routeProvider: ng.route.IRouteProvider) {
+            $routeProvider
+                .when('/', {
+                    template: 'Hello World!'
+                });
+        });
+}
